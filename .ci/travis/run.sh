20fc125c90d14d666d8dc675eaae5458f07efe42
Vardan Pogosian <vardan.pogosyan@gmail.com>
2018-07-11 14:58:50 +0300

    fix(ci): set PYENV_ROOT in run.sh

--- .ci/travis/run.sh
+++ .ci/travis/run.sh
@@ -3,8 +3,11 @@
 set -e
 set -x
 
-PYVER=`python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 
+
+PYVER=`python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))'`
 
 # setup OSX
 if which pyenv > /dev/null; then
